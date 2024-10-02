cc614bba9cbbf1db42578f23eee4914e08e9074d
UsamaWalayat12 <163966641+UsamaWalayat12@users.noreply.github.com>
2024-10-02 19:29:52 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,28 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Aboutpage from "./pages/Aboutpage";
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
-
-import "./App.css";
 import Applayout from "./pages/Applayout";
 import Contactpage from "./pages/Contactpage";
-import Loginpage from "./pages/Loginpage";
+import Loginpage from "./pages/Loginpage";  // Make sure this is correctly named
 import Registerpage from "./pages/Registerpage";
+import "./App.css";
 
 const App = () => {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="app" element={<Applayout />} />
-          <Route path="contact" element={<Contactpage />} />
-          <Route path="about" element={<Aboutpage />} />
-          <Route path="signin" element={<Loginpage />} />
-          <Route path="register" element={<Registerpage />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="app" element={<Applayout />} />
+        <Route path="contact" element={<Contactpage />} />
+        <Route path="about" element={<Aboutpage />} />
+        <Route path="signin" element={<Loginpage />} />   {/* Ensure the path matches your link */}
+        <Route path="register" element={<Registerpage />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
 export default App;
+
