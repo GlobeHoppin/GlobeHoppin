c0bb8d2d55ec8a9703574468079f6527a1703f82
Rajashekar Reddy gunda <Enter your emailid here>
2024-10-13 14:16:25 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Aboutpage from "./pages/Aboutpage";
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
-
-import "./App.css";
 import Applayout from "./pages/Applayout";
 import Contactpage from "./pages/Contactpage";
 import Loginpage from "./pages/Loginpage";
 import Registerpage from "./pages/Registerpage";
 
+import "./App.css";
+
 const App = () => {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="app" element={<Applayout />} />
-          <Route path="contact" element={<Contactpage />} />
-          <Route path="about" element={<Aboutpage />} />
-          <Route path="signin" element={<Loginpage />} />
-          <Route path="register" element={<Registerpage />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="app" element={<Applayout />} />
+        <Route path="contact" element={<Contactpage />} />
+        <Route path="about" element={<Aboutpage />} />
+        <Route path="signin" element={<Loginpage />} />
+        <Route path="register" element={<Registerpage />} />
+        <Route path="/not-found" element={<PageNotFound />} />
+        {/* Redirect unknown routes to not-found page */}
+        <Route path="*" element={<Navigate to="/not-found" replace />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
