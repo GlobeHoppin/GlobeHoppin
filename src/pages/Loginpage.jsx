1aeee911aadbe7bf962fb7488482bb81a5d6e549
Suresh <sureshb9a11460@gmail.com>
2024-10-15 15:27:35 +0530

    enhancement

--- src/pages/Loginpage.jsx
+++ src/pages/Loginpage.jsx
@@ -35,8 +35,8 @@ function Loginpage() {
             <div className="h-screen flex md:block justify-center items-center">
               <section className="bg-gray-50 dark:bg-gray-900">
                 <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                  <div className="w-full bg-white sm:px-8 md:px-0  rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                  <div className="w-full bg-white sm:px-9  md:px-0  rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                    <div className="p-10 space-y-4 md:space-y-6 sm:p-8">
                       <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                         Sign in to your account
                       </h1>
@@ -49,7 +49,7 @@ function Loginpage() {
                             type="email"
                             name="email"
                             id="email"
-                            className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block sm:w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             placeholder="name@email.com"
                             required=""
                           />
@@ -63,11 +63,11 @@ function Loginpage() {
                             name="password"
                             id="password"
                             placeholder="••••••••"
-                            className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block sm:w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             required=""
                           />
                         </div>
-                        <div className="flex items-center justify-between">
+                        <div className="flex flex-row  items-center flex-wrap space-y-2 gap-x-2 justify-between">
                           <div className="flex items-start">
                             <div className="flex items-center h-5">
                               <input
@@ -84,12 +84,14 @@ function Loginpage() {
                               </label>
                             </div>
                           </div>
-                          <a
-                            href="#"
-                            className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
-                          >
-                            Forgot password?
-                          </a>
+                          <div>
+                            <a
+                              href="#"
+                              className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
+                            >
+                              Forgot password?
+                            </a>
+                          </div>
                         </div>
                         <button
                           type="submit"
--- src/pages/Registerpage.jsx
+++ src/pages/Registerpage.jsx
@@ -47,7 +47,7 @@ function Registerpage() {
                           type="email"
                           name="email"
                           id="email"
-                          className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                          className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block  sm:w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 "
                           placeholder="name@email.com"
                           required=""
                         />
@@ -61,7 +61,7 @@ function Registerpage() {
                           name="password"
                           id="password"
                           placeholder="••••••••"
-                          className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                          className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block sm:w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                           required=""
                         />
                       </div>
@@ -74,7 +74,7 @@ function Registerpage() {
                           name="confirm-password"
                           id="confirm-password"
                           placeholder="••••••••"
-                          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                          className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block sm:w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                           required=""
                         />
                       </div>
