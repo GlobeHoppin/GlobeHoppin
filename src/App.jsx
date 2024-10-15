ddcd4f5a1cef38ed617d2fbec36f5f9c533a7f88
Vaishnavi Patil <109457185+Vaishh18@users.noreply.github.com>
2024-10-15 23:09:31 +0530

    Added privacy policy and terms of use pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Aboutpage from "./pages/Aboutpage";
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
-
+import TermsOfUse from "./pages/TermsOfUse";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
 import "./App.css";
 import Applayout from "./pages/Applayout";
 import Contactpage from "./pages/Contactpage";
@@ -20,6 +21,8 @@ const App = () => {
           <Route path="about" element={<Aboutpage />} />
           <Route path="signin" element={<Loginpage />} />
           <Route path="register" element={<Registerpage />} />
+          <Route path="/Terms" element={<TermsOfUse/>}/>
+          <Route path="/PrivacyPolicy" element={<PrivacyPolicy/>}/>
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
