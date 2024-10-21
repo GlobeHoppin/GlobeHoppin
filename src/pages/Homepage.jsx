da6c5cf6999c2d45af3b5e8fb38a271320e02ce6
sivasai <sivasainookala@gmail.com>
2024-10-21 16:53:40 +0530

    changes have been added

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -106,7 +106,7 @@ function Homepage() {
                 </NavLink>
                 <NavLink
                   to="/register"
-                  className="rounded-md px-3.5 py-2.5 text-sm font-semibold text-gray-900 border border-gray-900 hover:border-gray-500"
+                  className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   Register
                 </NavLink>
