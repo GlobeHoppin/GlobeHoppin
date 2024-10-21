b19e668de8f4bbad73090f6e63646cd89b6cc58c
Aswani Bolisetti <117751107+AswaniBolisetti@users.noreply.github.com>
2024-10-21 21:11:21 +0530

    added load more feature to FAQs (#92)
    * added load more faqs as a seperate page

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Aboutpage from "./pages/Aboutpage";
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
-import TermsOfUse from "./pages/TermsOfUse";
-import PrivacyPolicy from "./pages/PrivacyPolicy";
-import "./App.css";
 import Applayout from "./pages/Applayout";
 import Contactpage from "./pages/Contactpage";
 import BookingPage from "./pages/booking";
@@ -14,7 +11,10 @@ import Famous from "./pages/famous";
 import YourBookings from "./pages/YourBookings";
 import Loginpage from "./pages/Loginpage";
 import Registerpage from "./pages/Registerpage";
-import TripBudgetCalculator from "./pages/TripBudgetCalculatorPage";
+
+import FAQPage from "./pages/faq"; // Import the FAQPage component
+import "./App.css";
+
 
 const App = () => {
   return (
@@ -33,7 +33,11 @@ const App = () => {
           <Route path="about" element={<Aboutpage />} />
           <Route path="signin" element={<Loginpage />} />
           <Route path="register" element={<Registerpage />} />
-          <Route path="trip-budget" element={<TripBudgetCalculator />} />
+
+
+          <Route path="faq" element={<FAQPage />} /> {/* Add the FAQPage route */}
+
+
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
