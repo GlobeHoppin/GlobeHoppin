ea9a61bacb3c29566c67b20cc34c21a696c4fead
Jacob Chaar <jacobchaar@live.ca>
2024-10-22 20:39:24 -0400

    Refactor PageNav component to remove unnecessary code

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -19,15 +19,12 @@ const navigation = [
   { name: "About", href: "/about", current: false },
   { name: "Mark Your Journey", href: "/app", current: false },
   { name: "Contact", href: "/contact", current: false },
-<main>
   { name: "BookingPage", href: "/BookingPage", current: false },
   
   { name: "YourBookings", href: "/YourBookings", current: false },
   { name: "Guide", href: "/Famous", current: false },
   
-
   { name: "Trip Budget Calculator", href: "/trip-budget", current: false },
-</main>
 ];
 
 function classNames(...classes) {
@@ -37,28 +34,11 @@ function classNames(...classes) {
 function PageNav() {
   const isSignedIn = false; // Placeholder for sign-in status
   return (
-    <>
 
-      <Disclosure as="nav" className="bg-slate-950 absolute inset-x-0 top-0 z-50">
-
-      {/* <nav>
-        <ul>
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/about">About</NavLink>
-          </li>
-          <li>
-            <NavLink to="/contact">Contact</NavLink>
-          </li>
-        </ul>
-      </nav> */}
       <Disclosure
         as="nav"
         className="bg-slate-950 fixed inset-x-0 top-0 z-50"
       >
-
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
           <div className="relative flex h-16 items-center justify-between">
             <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
@@ -203,7 +183,6 @@ function PageNav() {
           </div>
         </DisclosurePanel>
       </Disclosure>
-    </>
   );
 }
 
