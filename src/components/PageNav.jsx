7c1c8eb48ed4dcd9403ab2416a015063e508355a
Mustafiz04 <kaifee000@gmail.com>
2024-12-07 23:55:47 +0530

    fix style for globe

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -127,7 +127,7 @@ function PageNav() {
                           to=""
                           className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
                         >
-                          Sign out
+                          Logout
                         </NavLink>
                       </Menu.Item>
                     </Menu.Items>
--- src/pages/Applayout.jsx
+++ src/pages/Applayout.jsx
@@ -121,43 +121,49 @@ function Applayout() {
           }
           <section className="py-8">
             <div className="mx-auto max-w-screen-xl">
-              <div className="grid md:grid-cols-3 gap-2">
+              <div className="flex flex-col w-full gap-2">
                 <div
-                  className="col-span-2 bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg"
+                  className="flex-1 bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg"
                   style={{ width: "100%", height: "100%" }}
                 >
                   <MapBox />
                 </div>
-                <div
-                  className="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12 overflow-y-auto h-96"
-                  style={{ height: "600px" }}
-                >
-                  <div className="space-y-2">
-                    {
-                      user && user.pins ? (
+                {user && user.pins && user.pins.length > 0 ? (
+                  <div
+                    className="flex-1 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12 overflow-y-auto"
+                    style={{ height: "600px" }}
+                  >
+                    <div className="space-y-2 grid grid-cols-1 sm:grid-cols-3 gap-4">
+                      {
                         user.pins.map((pin, index) => (
-                          <div key={index} className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                            <a href="#">
-                              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                {pin.title}
-                              </h5>
-                            </a>
-                            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                              {pin.address}
-                            </p>
-                            <a
-                              href="#"
-                              className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                            >
-                              Read more
-                            </a>
+                          <div key={index} className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 grid grid-cols-3 gap-4">
+                            <div className="col-span-2">
+                              <a href="#">
+                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                  {pin.title}
+                                </h5>
+                              </a>
+                              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                                {pin.address}
+                              </p>
+                            </div>
+                            <div className="flex items-center justify-center">
+                              <a
+                                href="#"
+                                className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                              >
+                                Read more
+                              </a>
+                            </div>
                           </div>
                         ))
-                      ) : <div></div>
-                    }
-                    
+                      }
+                    </div>
                   </div>
-                </div>
+                ) : (
+                  <></>
+                )
+                }
               </div>
             </div>
           </section>
